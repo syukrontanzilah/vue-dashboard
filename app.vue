40f68b5456c6c072595c3c1f3c4eeea97364a7d1
syukrontanzilah <syukrontanzilah@gmail.com>
2024-07-31 09:40:16 +0700

    init

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+<NuxtLayout>
+  halo lagi apa kamu
+  <NuxtPage/>
+</NuxtLayout>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const loading = ref(false)
+</script>
+
+<template>
+    <div>
+        <Button>Button</Button>
+        
+    </div>
+</template>
+
+
+
+
